bd2f3408fe466f6c47a99e860c94e81d0a50965a
Sreerag P <sreeragp276@gmail.com>
2024-01-17 22:41:31 +0530

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AIComp from "./components/pages/AIComp";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
         <AIComp />
+        <Analytics/>
     </div>
   );
 }
