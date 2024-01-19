9f02dce0dd2aef20d0373f8a28cc402f8abfa40f
Sreerag P <sreeragp276@gmail.com>
2024-01-19 15:50:02 +0530

    Styling

--- src/components/AIComp.jsx
+++ src/components/AIComp.jsx
@@ -131,7 +131,7 @@ const AIComp = () => {
           <h2 className="mt-5 text-4xl font-extrabold p-4 leading-[1.15] text-black sm:text-3xl text-center">
             Recommended Destination:
           </h2>
-          <div className="summary_box prose lg:prose-lg xl:prose-xl 2xl:prose-xl text-gray-700">
+          <div className="summary_box prose sm:prose-sm lg:prose-lg xl:prose-xl 2xl:prose-2xl max-w-[1000px] text-left text-gray-700">
             <ReactMarkdown>{destination}</ReactMarkdown>
           </div>
         </div>
